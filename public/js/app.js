9d7f896ec87f31844c58fee18beaa5907ed13e04
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2021-10-28 11:35:08 -0500

    Updated files

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,42 @@
-"use strict";
+'use strict';
 
-const weatherForm = document.querySelector("form");
-const search = document.querySelector("input");
-const messageOne = document.querySelector("#message-one");
-const messageTwo = document.querySelector("#message-two");
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const iconContainer = document.querySelector('#icon');
+const messageOne = document.querySelector('#message-one');
+const messageTwo = document.querySelector('#message-two');
 
-weatherForm.addEventListener("submit", (e) => {
-	e.preventDefault();
-	messageOne.textContent = "";
-	messageTwo.textContent = "";
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  messageOne.textContent = '';
+  messageTwo.textContent = '';
 
-	const location = search.value;
+  const location = search.value;
 
-	if (!location) {
-		messageOne.textContent = "Must submit a valid location!";
-	} else {
-		fetch(`/weather?address=${location}`).then((res) => {
-			res.json().then((data) => {
-				if (data.err) {
-					messageOne.textContent = data.err;
-				} else {
-					messageOne.textContent = data.location;
-					messageTwo.textContent = data.forecast;
-				}
-			});
-		});
-	}
+  if (!location) {
+    messageOne.textContent = 'Must submit a valid location!';
+  } else {
+    fetch(`/weather?address=${location}`).then((res) => {
+      res.json().then((data) => {
+        console.log(data);
+        if (data.err) {
+          messageOne.textContent = data.err;
+        } else {
+          if (iconContainer.hasChildNodes()) {
+            iconContainer.removeChild(iconContainer.lastElementChild);
+          }
 
-	// console.log(location);
+          const icon = document.createElement('img');
+          icon.src = data.forecast.icon;
+          icon.setAttribute('class', 'icon');
+          iconContainer.appendChild(icon);
+
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast.description;
+        }
+      });
+    });
+  }
+
+  // console.log(location);
 });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,20 +1,21 @@
-const request = require("request");
+const baseModule = require('hbs');
+const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-	const url = `http://api.weatherstack.com/current?access_key=bdea3b4f15616a8dd241b0d245618704&query=${latitude},${longitude}`;
+  const url = `http://api.weatherstack.com/current?access_key=bdea3b4f15616a8dd241b0d245618704&query=${latitude},${longitude}`;
 
-	request({ url, json: true }, (err, { body }) => {
-		if (err) {
-			callback("Unable to connect to weather service!", undefined);
-		} else if (body.error) {
-			callback("Unable to find location. Try again.", undefined);
-		} else {
-			callback(
-				undefined,
-				`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`
-			);
-		}
-	});
+  request({ url, json: true }, (err, { body }) => {
+    if (err) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location. Try again.', undefined);
+    } else {
+      callback(undefined, {
+        description: `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}.`,
+        icon: body.current.weather_icons[0],
+      });
+    }
+  });
 };
 
 module.exports = forecast;
